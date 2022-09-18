741d5705f6b0c8fe4ed9fa0f33b4b41d7369b98c
Artur Siedlerski <don.siedler@gmail.com>
2022-09-18 17:53:18 +0200

    Add Address model and update User model

--- dating/models.py
+++ dating/models.py
@@ -2,6 +2,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Address(models.Model):
+    city = models.CharField(max_length=30)
+    post_code = models.CharField(max_length=6)
+    street = models.CharField(max_length=64, null=True)
+
+    def __str__(self):
+        return f"{self.post_code}, {self.city}, {self.street}"
+
+
 class User(AbstractUser):
     GENDERS = (
         (0, "female"),
@@ -14,6 +23,7 @@ class User(AbstractUser):
     photo = models.ImageField(upload_to="dating/static_files/uploads/",
                               default="dating/static_files/default_avatar.png")
     phone = models.CharField(max_length=12, null=True)
+    address = models.ForeignKey(Address, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.username
