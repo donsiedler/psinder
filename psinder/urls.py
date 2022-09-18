98a5b54f8b0e6f95c6b9fdc368331f34981a191f
Artur Siedlerski <don.siedler@gmail.com>
2022-09-18 16:53:48 +0200

    Home page draft

--- psinder/urls.py
+++ psinder/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from dating import views as dating
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', dating.MainPageView.as_view(), name="home-page"),
 ]
