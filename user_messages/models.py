96930a23599b2482b979bb91e458ac51f1bfd046
Artur Siedlerski <don.siedler@gmail.com>
2022-10-01 15:51:35 +0200

    Add Thread and Message model

--- user_messages/models.py
+++ user_messages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Thread(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+
+
+class Message(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, blank=True, null=True)
+    sender_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    recipient_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
