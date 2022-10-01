20bf9b676e24acc886c5b2affcb8be8f26711787
Artur Siedlerski <don.siedler@gmail.com>
2022-10-01 15:58:36 +0200

    Add ThreadsList view

--- user_messages/views.py
+++ user_messages/views.py
@@ -1,3 +1,14 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Thread, Message
+
+
+class ThreadsList(View):
+    def get(self, request, *args, **kwargs):
+        threads = Thread.objects.filter(Q(sender=request.user) | Q(recipient=request.user))
+        context = {
+            "threads": threads,
+        }
+        return render(request, "user_messages/inbox.html", context)
